73279318f19ac0efc974d3c26bda466fccbf2606
djc5166 <djc5166@gmail.com>
2023-04-29 13:09:30 -0400

    Project cleanup and renaming
    Added WH40K:SM info to README.md
    Remove directxsdk vcpkg

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 DINPUT8 DLL for hooking GetSystemInfo() to fix the number of processors reported to the application at 12
 
 ## Fixes (AFAIK)
-* Darksiders (Original)
-* LostPlanet2 (Non-steam Version)
+- 12 max logical cores
+    - Darksiders (Original)
+    - LostPlanet2 (Non-steam Version)
+
+- 26 max logical cores
+    - Warhammer 40,000: Space Marine
 
 ## Requirements
 
 - [VS2022 Community](https://visualstudio.microsoft.com/vs/community/)
 - [vcpkg](https://github.com/microsoft/vcpkg)
-    - [directxsdk    jun10\#6 \(x86\-windows\-static\)](https://walbourn.github.io/where-is-the-directx-sdk-2021-edition/)
-    - [minhook       1\.3\.3\#3 \(x86\-windows\-static\)](https://github.com/TsudaKageyu/minhook)
-
+    - [minhook v1\.3\.3\#3 \(x86\-windows\-static\)](https://github.com/TsudaKageyu/minhook)
--- dllmain.cpp
+++ dllmain.cpp
@@ -1,72 +1,80 @@
 #define DIRECTINPUT_VERSION 0x0800
+#define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
+#include <Windows.h>
+
 #include <dinput.h>
 #include <tchar.h>
 #include <MinHook.h>
 
-constexpr auto MAXCORES = 12;
+constexpr DWORD MAX_CORES = 12;
 
-typedef	LPCDIDATAFORMAT(WINAPI* GetdfDIJoystickProc)    ();
-typedef         HRESULT(WINAPI* DirectInput8CreateProc) (HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);
-typedef         HRESULT(WINAPI* DllCanUnloadNowProc)    ();
-typedef         HRESULT(WINAPI* DllGetClassObjectProc)  (REFCLSID, REFIID, LPVOID*);
-typedef         HRESULT(WINAPI* DllRegisterServerProc)  ();
-typedef         HRESULT(WINAPI* DllUnregisterServerProc)();
-typedef           void (WINAPI* GetSystemInfoProc)      (LPSYSTEM_INFO);
+//dinput8
+typedef LPCDIDATAFORMAT(WINAPI* GetdfDIJoystickProc)(VOID);
+typedef HRESULT(WINAPI* DirectInput8CreateProc)(HINSTANCE, DWORD, REFIID, LPVOID*, LPUNKNOWN);
+typedef HRESULT(WINAPI* DllCanUnloadNowProc)(VOID);
+typedef HRESULT(WINAPI* DllGetClassObjectProc)(REFCLSID, REFIID, LPVOID*);
+typedef HRESULT(WINAPI* DllRegisterServerProc)(VOID);
+typedef HRESULT(WINAPI* DllUnregisterServerProc)(VOID);
 
-GetdfDIJoystickProc     fpGetdfDIJoystick = nullptr;
-DirectInput8CreateProc  fpDirectInput8Create = nullptr;
-DllCanUnloadNowProc     fpDllCanUnloadNow = nullptr;
-DllGetClassObjectProc   fpDllGetClassObject = nullptr;
-DllRegisterServerProc   fpDllRegisterServer = nullptr;
-DllUnregisterServerProc fpDllUnregisterServer = nullptr;
-GetSystemInfoProc       fpGetSystemInfo = nullptr;
-HMODULE                 hDinput8dll = nullptr;
+GetdfDIJoystickProc oGetdfDIJoystick = nullptr;
+DirectInput8CreateProc oDirectInput8Create = nullptr;
+DllCanUnloadNowProc oDllCanUnloadNow = nullptr;
+DllGetClassObjectProc oDllGetClassObject = nullptr;
+DllRegisterServerProc oDllRegisterServer = nullptr;
+DllUnregisterServerProc oDllUnregisterServer = nullptr;
 
-void WINAPI hkGetSystemInfo(LPSYSTEM_INFO siSysInfo)
+//kernel32
+typedef VOID(WINAPI* GetSystemInfoProc)(LPSYSTEM_INFO);
+
+GetSystemInfoProc oGetSystemInfo = nullptr;
+
+//dinput8 handle
+HMODULE hDinput8dll = nullptr;
+
+VOID WINAPI hkGetSystemInfo(LPSYSTEM_INFO lpSystemInfo)
 {
-    fpGetSystemInfo(siSysInfo);
+    oGetSystemInfo(lpSystemInfo);
 
-    if (siSysInfo->dwNumberOfProcessors > MAXCORES)
+    if (lpSystemInfo->dwNumberOfProcessors > MAX_CORES)
     {
-        siSysInfo->dwNumberOfProcessors = MAXCORES;
-        siSysInfo->dwActiveProcessorMask = ((1 << MAXCORES) - 1);
+        lpSystemInfo->dwNumberOfProcessors = MAX_CORES;
+        lpSystemInfo->dwActiveProcessorMask = ((1 << MAX_CORES) - 1);
     }
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-    switch (ul_reason_for_call)
+    switch (fdwReason)
     {
-    case DLL_PROCESS_ATTACH:
-    {
-        if (MH_Initialize() == MH_OK)
+        case DLL_PROCESS_ATTACH:
         {
-            MH_CreateHookApi(TEXT("kernel32"), "GetSystemInfo", &hkGetSystemInfo, reinterpret_cast<LPVOID*>(&fpGetSystemInfo));
-            MH_EnableHook(MH_ALL_HOOKS);
+            if (MH_Initialize() == MH_OK)
+            {
+                MH_CreateHookApi(L"kernel32", "GetSystemInfo", &hkGetSystemInfo, reinterpret_cast<LPVOID*>(&oGetSystemInfo));
+                MH_EnableHook(MH_ALL_HOOKS);
+            }
+            break;
         }
-        break;
-    }
-    case DLL_PROCESS_DETACH:
-    {
-        if (hDinput8dll != nullptr)
-            FreeLibrary(hDinput8dll);
+        case DLL_PROCESS_DETACH:
+        {
+            if (hDinput8dll != nullptr)
+                FreeLibrary(hDinput8dll);
 
-        MH_Uninitialize();
-        break;
-    }
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    default:
-    {
-        break;
-    }
+            MH_Uninitialize();
+            break;
+        }
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        default:
+        {
+            break;
+        }
     }
     return TRUE;
 }
 
-void InitDI8()
+VOID InitDI8(VOID)
 {
     if (hDinput8dll != nullptr)
         return;
@@ -78,74 +86,70 @@ void InitDI8()
     hDinput8dll = LoadLibrary(sysPath);
 }
 
-LPCDIDATAFORMAT WINAPI GetdfDIJoystick()
+LPCDIDATAFORMAT WINAPI GetdfDIJoystick(VOID)
 {
-    InitDI8();
-    if (fpGetdfDIJoystick == nullptr)
+    if (oGetdfDIJoystick == nullptr)
     {
-        fpGetdfDIJoystick = reinterpret_cast<GetdfDIJoystickProc>(GetProcAddress(hDinput8dll, "GetdfDIJoystick"));
-        if (fpGetdfDIJoystick == nullptr)
+        oGetdfDIJoystick = reinterpret_cast<GetdfDIJoystickProc>(GetProcAddress(hDinput8dll, "GetdfDIJoystick"));
+        if (oGetdfDIJoystick == nullptr)
             return nullptr;
     }
-    return fpGetdfDIJoystick();
+    return oGetdfDIJoystick();
 }
 
 HRESULT WINAPI DirectInput8Create(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID* ppvOut, LPUNKNOWN punkOuter)
 {
     InitDI8();
-    if (fpDirectInput8Create == nullptr)
+    if (oDirectInput8Create == nullptr)
     {
-        fpDirectInput8Create = reinterpret_cast<DirectInput8CreateProc>(GetProcAddress(hDinput8dll, "DirectInput8Create"));
-        if (fpDirectInput8Create == nullptr)
+        oDirectInput8Create = reinterpret_cast<DirectInput8CreateProc>(GetProcAddress(hDinput8dll, "DirectInput8Create"));
+        if (oDirectInput8Create == nullptr)
             return E_FAIL;
     }
-    return fpDirectInput8Create(hinst, dwVersion, riidltf, ppvOut, punkOuter);
+    return oDirectInput8Create(hinst, dwVersion, riidltf, ppvOut, punkOuter);
 }
 
-HRESULT WINAPI DllCanUnloadNow()
+STDAPI DllCanUnloadNow(VOID)
 {
-    InitDI8();
-    if (fpDllCanUnloadNow == nullptr)
+    if (oDllCanUnloadNow == nullptr)
     {
-        fpDllCanUnloadNow = reinterpret_cast<DllCanUnloadNowProc>(GetProcAddress(hDinput8dll, "DllCanUnloadNow"));
-        if (fpDllCanUnloadNow == nullptr)
+        oDllCanUnloadNow = reinterpret_cast<DllCanUnloadNowProc>(GetProcAddress(hDinput8dll, "DllCanUnloadNow"));
+        if (oDllCanUnloadNow == nullptr)
             return E_FAIL;
     }
-    return fpDllCanUnloadNow();
+    return oDllCanUnloadNow();
 }
 
-HRESULT WINAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv)
 {
-    InitDI8();
-    if (fpDllGetClassObject == nullptr)
+    if (oDllGetClassObject == nullptr)
     {
-        fpDllGetClassObject = reinterpret_cast<DllGetClassObjectProc>(GetProcAddress(hDinput8dll, "DllGetClassObject"));
-        if (fpDllGetClassObject == nullptr)
+        oDllGetClassObject = reinterpret_cast<DllGetClassObjectProc>(GetProcAddress(hDinput8dll, "DllGetClassObject"));
+        if (oDllGetClassObject == nullptr)
             return E_FAIL;
     }
-    return fpDllGetClassObject(rclsid, riid, ppv);
+    return oDllGetClassObject(rclsid, riid, ppv);
 }
 
-HRESULT WINAPI DllRegisterServer()
+STDAPI DllRegisterServer(VOID)
 {
-    InitDI8();
-    if (fpDllRegisterServer == nullptr)
+    if (oDllRegisterServer == nullptr)
     {
-        fpDllRegisterServer = reinterpret_cast<DllRegisterServerProc>(GetProcAddress(hDinput8dll, "DllRegisterServer"));
-        if (fpDllRegisterServer == nullptr)
+        oDllRegisterServer = reinterpret_cast<DllRegisterServerProc>(GetProcAddress(hDinput8dll, "DllRegisterServer"));
+        if (oDllRegisterServer == nullptr)
             return E_FAIL;
     }
-    return fpDllRegisterServer();
+    return oDllRegisterServer();
 }
 
-HRESULT WINAPI DllUnregisterServer()
+STDAPI DllUnregisterServer(VOID)
 {
-    InitDI8();
-    if (fpDllUnregisterServer == nullptr)
+    if (oDllUnregisterServer == nullptr)
     {
-        fpDllUnregisterServer = reinterpret_cast<DllUnregisterServerProc>(GetProcAddress(hDinput8dll, "DllUnregisterServer"));
-        if (fpDllUnregisterServer == nullptr)
+        oDllUnregisterServer = reinterpret_cast<DllUnregisterServerProc>(GetProcAddress(hDinput8dll, "DllUnregisterServer"));
+        if (oDllUnregisterServer == nullptr)
             return E_FAIL;
     }
-    return fpDllUnregisterServer();
+    return oDllUnregisterServer();
 }
+
